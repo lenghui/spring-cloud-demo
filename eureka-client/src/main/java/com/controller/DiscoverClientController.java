d11c4e27e2663f4422622a55e02a93e733b715ad
lenghui <lengfeng@999999.com>
2018-09-14 19:08:45 +0800

    commit

---
+++ eureka-client/src/main/java/com/controller/DiscoverClientController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiscoverClientController {
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@GetMapping("/dc")
+	public String discovery() {
+		String services = "services:"+discoveryClient.getServices();
+		System.out.println(services);
+		return services;
+	}
+}
