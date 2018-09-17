907a47f9f1ffc33452ccd577d8b945381fff64c1
lenghui <lengfeng@999999.com>
2018-09-17 15:22:41 +0800

    增加分布式配置demo

---
+++ config-server-git/src/main/java/com/App.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class App {
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(App.class).web(true).run(args);
+	}
+}
